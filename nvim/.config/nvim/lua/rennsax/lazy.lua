1d07ba0baffe9ce7b564823922c25d4b50ab025e
Bojun Ren <bj.ren.coding@outlook.com>
2024-02-13 14:04:20 +0800

    nvim: add folke/flash.nvim plugin
    I'd like to use `f` as my flash key, and disable some features
    currently. Maybe someday I would make a complete learn about the plugin.

--- nvim/.config/nvim/lua/rennsax/lazy.lua
+++ nvim/.config/nvim/lua/rennsax/lazy.lua
@@ -22,5 +22,33 @@ require("lazy").setup({
                 -- Configuration here, or leave empty to use defaults
             })
         end
-    }
+    },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {
+            modes = {
+                search = {
+                    enabled = false
+                },
+                char = {
+                    enabled = false,
+                    keys = {}
+                }
+            },
+            label = {
+                before = true,
+                after = false,
+            }
+        },
+        -- stylua: ignore
+        keys = {
+            { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+    },
 })
