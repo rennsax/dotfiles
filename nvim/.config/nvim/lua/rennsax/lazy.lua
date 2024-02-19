9581a53d71707cfe9d411cf660aab2e243f0b684
Bojun Ren <bj.ren.coding@outlook.com>
2024-02-19 23:11:23 +0800

    nvim: fix the shortcut conflict
    nvim-surround and flash.nvim both use `S` key in visual mode.
    Disable `S` key in visual mode for flash.nvim.

--- nvim/.config/nvim/lua/rennsax/lazy.lua
+++ nvim/.config/nvim/lua/rennsax/lazy.lua
@@ -42,10 +42,11 @@ require("lazy").setup({
                 after = false,
             }
         },
+        -- See `:help map-table` for the meaning of each abbreviation option.
         -- stylua: ignore
         keys = {
             { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
             { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
             { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
             { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
