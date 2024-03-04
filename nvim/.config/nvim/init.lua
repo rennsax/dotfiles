bc3b8b9ef0c74eccda7cde596a3199b3a32c42c7
Bojun Ren <bj.ren.coding@outlook.com>
2024-03-04 19:43:50 +0800

    nvim: remove unuseful keybinding, add comments

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,24 +7,30 @@ if vim.g.vscode then
     -- See https://github.com/vscode-neovim/vscode-neovim/blob/68f056b4c9cb6b2559baa917f8c02166abd86f11/vim/vscode-code-actions.vim#L93-L95
     vim.api.nvim_set_keymap("n", "<Up>", "gk", {})
     vim.api.nvim_set_keymap("n", "<Down>", "gj", {})
+    -- show hover
     vim.api.nvim_set_keymap(
-        "n", "<Leader>t", ":lua require('vscode-neovim').call('workbench.action.terminal.focus')<CR>", {})
+        "n", "<Leader>h", ":lua require('vscode-neovim').call('editor.action.showHover')<CR>", {})
+    -- code refactor
     vim.api.nvim_set_keymap(
-        "n", "<Leader>h", ":lua require('vscode-neovim').action('editor.action.showHover')<CR>", {})
-    vim.api.nvim_set_keymap(
-        "n", "<Leader>r", ":lua require('vscode-neovim').action('editor.action.refactor')<CR>", {})
+        "n", "<Leader>cr", ":lua require('vscode-neovim').call('editor.action.refactor')<CR>", {})
     vim.api.nvim_set_keymap(
         "n", "<Leader>o", ":lua require('vscode-neovim').call('clangd.switchheadersource')<CR>", {})
+    -- update tag
     vim.api.nvim_set_keymap(
-        "n", "<Leader><leader>t", ":lua require('vscode-neovim').call('editor.emmet.action.updateTag')<CR>", {})
+        "n", "<Leader>ut", ":lua require('vscode-neovim').call('editor.emmet.action.updateTag')<CR>", {})
+    -- show definition
     vim.api.nvim_set_keymap(
-        "n", "<Leader><leader>d", ":lua require('vscode-neovim').call('editor.action.peekDefinition')<CR>", {})
+        "n", "<Leader>sd", ":lua require('vscode-neovim').call('editor.action.peekDefinition')<CR>", {})
+    -- show reference
     vim.api.nvim_set_keymap(
-        "n", "<Leader><leader>r", ":lua require('vscode-neovim').call('editor.action.referenceSearch.trigger')<CR>", {})
+        "n", "<Leader>sr", ":lua require('vscode-neovim').call('editor.action.referenceSearch.trigger')<CR>", {})
+    -- toggle dash
     vim.api.nvim_set_keymap(
         "n", "<Leader><C-h>", ":lua require('vscode-neovim').call('extension.dash.specific')<CR>", {})
+    -- toggle bookmark
     vim.api.nvim_set_keymap(
         "n", "<Leader>m", ":lua require('vscode-neovim').call('bookmarks.toggle')<CR>", {})
+    -- open file manage
     vim.api.nvim_set_keymap(
         "n", "<Leader>f", ":lua require('vscode-neovim').call('workbench.explorer.fileView.focus')<CR>", {})
 end
