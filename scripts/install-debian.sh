0cea00c0160a81ef0141a115a898b6984720b2de
Bojun Ren <bj.ren.coding@outlook.com>
2024-03-07 14:01:55 +0800

    script: add verbose mode for install-debian.sh

--- scripts/install-debian.sh
+++ scripts/install-debian.sh
@@ -18,13 +18,21 @@ alter_apt_sources() {
     esac
 }
 
-if [ "${DEBIAN_NOT_SJTU_SOURCES:-0}" -ne 1 ]; then
+: "${INSTALL_DEBIAN_NO_SJTU_SOURCES:=0}"
+: "${INSTALL_DEBIAN_TRACE:=0}"
+
+if [ "${INSTALL_DEBIAN_NO_SJTU_SOURCES}" -ne 1 ]; then
     alter_apt_sources || error_exit "failed to alter apt sources"
 fi
 
 info "update packages"
-sudo apt-get update -y >/dev/null
-sudo apt-get upgrade -y >/dev/null
+if [ "${INSTALL_DEBIAN_TRACE}" -eq 1 ]; then
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+else
+    sudo apt-get update -y >/dev/null
+    sudo apt-get upgrade -y >/dev/null
+fi
 
 PACKAGES="man zsh git xz-utils curl"
 PACKAGES="${PACKAGES} fzf neovim fd-find tree lua5.4"
@@ -33,8 +41,13 @@ PACKAGES="${PACKAGES} $(printf "%s" "$DEBIAN_EXTRA_PACKAGES" | tr ':' ' ')"
 IFS=" "
 for package in $PACKAGES; do
     info "installing package: $package..."
-    int_exit_eval sudo apt-get install "$package" -y >/dev/null \
-        || error_report "failed to install $package"
+    if [ "${INSTALL_DEBIAN_TRACE}" -eq 1 ]; then
+        int_exit_eval sudo apt-get install "$package" -y \
+            || error_report "failed to install $package"
+    else
+        int_exit_eval sudo apt-get install "$package" >/dev/null -y \
+            || error_report "failed to install $package"
+    fi
 done
 
 has fdfind && ln -sf "$(which fdfind)" ~/.local/bin/fd
