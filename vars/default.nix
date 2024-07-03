80e35a2d245cf55d23066374bd1c4ddb916d448e
Bojun Ren <bj.ren.coding@outlook.com>
2024-07-03 11:28:55 +0800

    Use dummy nixConfigDir value in vars

--- vars/default.nix
+++ vars/default.nix
@@ -3,7 +3,7 @@
   me = import ./me.nix;
   network = import ./network.nix;
   # Parent directory of the whole Nix flake. Must be hard-coded.
-  nixConfigDir = "$HOME/.dotfiles";
+  nixConfigDir = "/path/to/your/nix/config";
 
   inherit system;
   isDarwin = (builtins.elem system [ "aarch64-darwin" "x86_64-darwin" ]);
