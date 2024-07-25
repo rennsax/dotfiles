e0d2a911f82f4dfb0a93c56525c407a34a803aa2
Bojun Ren <bj.ren.coding@outlook.com>
2024-07-25 12:23:38 +0800

    chore(orb): add comments, refactor

--- modules/orbstack/orbstack.nix
+++ modules/orbstack/orbstack.nix
@@ -1,3 +1,13 @@
+# After opening Orbstack.app for the first time, it pollutes the system by:
+#
+#     1. Create symbolic links under /usr/local/bin
+#     2. Create ~/.orbstack and ~/.docker, if they do not exit.
+#     3. Create $ZDOTDIR/.zprofile and add scripts to initialize zsh completion.
+#
+# You can revert these changes if you don't like them.
+#
+# Since it's so impure, I do not include the orbstack package here. You should
+# manually install it.
 {
   pkgs,
   lib,
@@ -7,6 +17,11 @@
 with lib;
 let
   cfg = config.myModules.orbstack;
+
+  # Orbstack only provides completion for zsh.
+  shellInitFor = shell: ''
+    source ~/.orbstack/shell/init.${shell} 2>/dev/null || :
+  '';
 in
 {
   options.myModules.orbstack = {
@@ -18,12 +33,7 @@ in
 
   config = mkIf cfg.enable {
     # Put this before my personal configuration, so the orb.plugin.zsh can be correctly loaded.
-    programs.zsh.initExtraBeforeCompInit = (
-      mkOrder 200 ''
-        # Added by OrbStack: command-line tools and integration
-        source ~/.orbstack/shell/init.zsh 2>/dev/null || :
-      ''
-    );
+    programs.zsh.initExtraBeforeCompInit = mkIf cfg.enableZshIntegration (shellInitFor "zsh");
   };
 
 }
