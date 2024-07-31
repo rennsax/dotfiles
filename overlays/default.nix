76db0406ae7cb6d6008c01260be1cf08dc5d2c03
Bojun Ren <bj.ren.coding@outlook.com>
2024-07-31 16:31:23 +0800

    feat(font): add sarasa-term-sc-nerd (更纱黑体 nerd font 版本)

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,3 +1,6 @@
 { ... }: [
   (import ./cheat-fix-zsh-completion.nix)
+  (final: prev: {
+    sarasa-term-sc-nerd = prev.callPackage ./sarasa-term-sc-nerd.nix { };
+  })
 ]
---
+++ overlays/sarasa-term-sc-nerd.nix
@@ -0,0 +1,16 @@
+{ stdenvNoCC, fetchurl, ... }:
+stdenvNoCC.mkDerivation rec {
+  pname = "sarasa-term-sc-nerd";
+  version = "v1.1.0";
+  src = fetchurl {
+    url = "https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/${version}/sarasa-term-sc-nerd.ttf.tar.gz";
+    sha256 = "sha256-ADS5KTYQMTELd8MjAE+ugEwC5Gr8qDpN5kPirvgogAc=";
+  };
+  sourceRoot = ".";
+  buildPhase = ''
+    ls *.ttf
+  '';
+  installPhase = ''
+    find -name \*.ttf -exec mkdir -p $out/share/fonts/truetype/ScNerd \; -exec mv {} $out/share/fonts/truetype/ScNerd \;
+  '';
+}
