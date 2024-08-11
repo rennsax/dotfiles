acd31bae18618f8fffa75825a9f750c11c39cb7c
Bojun Ren <bj.ren.coding@outlook.com>
2024-08-11 15:33:27 +0800

    feat(overlay): add new package gnu-coding-standards via overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,6 +2,7 @@
   (import ./cheat-fix-zsh-completion.nix)
   (final: prev: {
     sarasa-term-sc-nerd = prev.callPackage ./sarasa-term-sc-nerd.nix { };
+    gnu-coding-standards = prev.callPackage ./gnu-coding-standards.nix { };
   })
   (import ./gn-install-emacs-lisp.nix)
 ]
---
+++ overlays/gnu-coding-standards.nix
@@ -0,0 +1,12 @@
+{ stdenvNoCC, fetchurl, ... }:
+stdenvNoCC.mkDerivation rec {
+  name = "gnu-coding-standards";
+  src = fetchurl {
+    url = "https://www.gnu.org/prep/standards/standards.info.tar.gz";
+    hash = "sha256-V1JBhsayLdOARQFXph0sEkQwXuHbt1xM7pV+ZzT4MwA=";
+  };
+  sourceRoot = ".";
+  installPhase = ''
+    install -vDt $out/share/info standards.info
+  '';
+}
