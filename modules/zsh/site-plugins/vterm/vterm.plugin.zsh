3bb4a7e83e9b244674133df245c436307e989973
Bojun Ren <bj.ren.coding@outlook.com>
2024-08-11 12:13:54 +0800

    feat(zsh/vterm): add vterm_cmd function for calling Elisp in vterm

--- modules/zsh/site-plugins/vterm/vterm.plugin.zsh
+++ modules/zsh/site-plugins/vterm/vterm.plugin.zsh
@@ -13,6 +13,20 @@ vterm_printf() {
     fi
 }
 
+# With vterm_cmd you can execute Emacs commands directly from the shell.
+# For example, vterm_cmd message "HI" will print "HI".
+# To enable new commands, you have to customize Emacs's variable
+# vterm-eval-cmds.
+vterm_cmd() {
+    local vterm_elisp
+    vterm_elisp=""
+    while [ $# -gt 0 ]; do
+        vterm_elisp="$vterm_elisp""$(printf '"%s" ' "$(printf "%s" "$1" | sed -e 's|\\|\\\\|g' -e 's|"|\\"|g')")"
+        shift
+    done
+    vterm_printf "51;E$vterm_elisp"
+}
+
 vterm_prompt_end() {
     # Use `VTERM_HOSTNAME` to override the hostname.
     vterm_printf "51;A$(whoami)@${VTERM_HOSTNAME:-$(hostname)}:$(pwd)"
