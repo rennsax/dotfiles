2cec95648f07a22c81e630d7925a94e40332a3c1
Bojun Ren <bj.ren.coding@outlook.com>
2024-08-12 14:29:50 +0800

    chore(dir-locals): auto-enable with-editor-mode for cheatsheets

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
 ((auto-mode-alist . (("zshrc-*" . sh-mode)
-                     ("modules/cheat/cheatsheets/*" . sh-mode))))
+                     ("modules/cheat/cheatsheets/*" . sh-mode)))
+ ("modules/cheat/cheatsheets" . ((nil . ((with-editor-mode . 1))))))
