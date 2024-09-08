fd2738637ca9196d83c3a0bb05b1a44b0c85e6dd
Bojun Ren <bj.ren.coding@outlook.com>
2024-09-08 22:03:37 +0800

    feat(emacs): better way to add simple lisp files
    Add gn-mode.el and cmake-mode.el without installing gn or cmake.
    
    Add emacs-clang-tools with specified LLVM version.

--- modules/emacs/emacs-deps.nix
+++ modules/emacs/emacs-deps.nix
@@ -5,6 +5,7 @@
   myVars,
   ...
 }:
+with lib;
 let
   cfg = config.myModules.emacs;
   myEmacs = pkgs.emacs29.override {
@@ -12,8 +13,36 @@ let
     # withNativeCompile = true;
     # withImageMagick = true;
   };
+
+  # Extract one file from src of DRV. FROM is a relative path for the file to extract.
+  # TO is a directory prefix to which the file should be copied to.
+  extractFileFromSrc =
+    {
+      drv,
+      from,
+      to,
+    }:
+    let
+      getBase = p: last (path.subpath.components p);
+      filename = getBase from;
+    in
+    pkgs.stdenvNoCC.mkDerivation {
+      pname = filename;
+      version = getVersion drv;
+      inherit (drv) src;
+      phases = "unpackPhase installPhase";
+      installPhase = ''
+        install -vDt $out/${to} ${from}
+      '';
+    };
+
+  mkSiteLisp =
+    drv: from:
+    (extractFileFromSrc {
+      inherit drv from;
+      to = "share/emacs/site-lisp";
+    });
 in
-with lib;
 {
   config = mkIf cfg.enable (mkMerge [
     # Generic dependencies.
@@ -50,14 +79,20 @@ with lib;
         let
           jinx-mod = callPackage ./jinx-mod.nix { };
           vterm-module = callPackage ./vterm-module.nix { };
-          emacs-clang-tools = callPackage ./emacs-clang-tools.nix { };
         in
         [
           jinx-mod
           vterm-module
-
-          emacs-clang-tools
         ];
+    }
+
+    # Lisp files extract from sources.
+    {
+      home.packages = with pkgs; [
+        (mkSiteLisp cmake "Auxiliary/cmake-mode.el")
+        (mkSiteLisp gn "misc/emacs/gn-mode.el")
+        (callPackage ./emacs-clang-tools.nix llvmPackages)
+      ];
     }
 
     # macOS dependencies.
