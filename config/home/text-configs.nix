671a697f87673b9906a768d8b280b9ea9ad7dcb7
Bojun Ren <bj.ren.coding@outlook.com>
2024-09-22 23:44:41 +0800

    feat(pip): add pip.conf
    Set the default index-url.

--- config/home/text-configs.nix
+++ config/home/text-configs.nix
@@ -7,4 +7,11 @@
       set disassembly-flavor intel
     '';
   };
+
+  xdg.configFile = {
+    "pip/pip.conf".text = ''
+      [global]
+      index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+    '';
+  };
 }
