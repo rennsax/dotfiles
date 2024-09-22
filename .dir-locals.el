8624485f6ca9a1cfb1419e976ca709e789a0d764
Bojun Ren <bj.ren.coding@outlook.com>
2024-09-22 23:45:32 +0800

    chore: do not enable with-editor-mode for cheatsheets

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,4 @@
 
 ((auto-mode-alist . (("zshrc-*" . sh-mode)
                      ("modules/cheat/cheatsheets/*" . sh-mode)))
- ("modules/cheat/cheatsheets" . ((nil . ((with-editor-mode . 1))))))
+ )
