3e94a7492539bac596e2e9e8f05830b661440867
Bojun Ren <bj.ren.coding@outlook.com>
2024-09-23 15:04:21 +0800

    chore(lib): remove unnecessary use of "myLib"
    Lesson learned: Avoid adding global APIs indiscriminately, as the function you
    desire might only be needed locally; Trust the power of the standard library, as
    sometimes the operations you expect can be easily implemented by the standard
    library rather than your own error-prone code.

--- lib/default.nix
+++ lib/default.nix
@@ -4,34 +4,6 @@
   myVars,
 }:
 {
-  # A simple wrapper for mkDerivation to combine multiple files into one directory.
-  mkCombinedDir =
-    args@{ name, files }:
-    pkgs.stdenvNoCC.mkDerivation (
-      let
-        srcs = map (file: file.src) files;
-        pairs = map (file: {
-          from = file.src;
-          to = file.name;
-        }) files;
-      in
-      rec {
-        inherit name;
-        phases = [
-          # "unpackPhase"
-          "buildPhase"
-        ];
-        # unpackPhase = ''
-        #   for _src in $srcs; do
-        #     cp "$_src" $(stripHash "$_src")
-        #   done
-        # '';
-        buildPhase = lib.concatStringsSep "\n" (
-          [ "mkdir -p $out" ] ++ (map ({ from, to }: "cp ${from} $out/${to}") pairs)
-        );
-      }
-    );
-
   # Make symbolic link towards the flake with a path or string.
   # Caveat: this function returns a derivation that may import impurity!
   _mkRelSymLink =
--- modules/hammerspoon/hammerspoon.nix
+++ modules/hammerspoon/hammerspoon.nix
@@ -2,7 +2,6 @@
   pkgs,
   lib,
   config,
-  myLib,
   ...
 }:
 with lib;
