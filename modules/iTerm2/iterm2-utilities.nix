406bc0f6e8f83ca07f08e63f9e226bde5073bfe1
Bojun Ren <bj.ren.coding@outlook.com>
2024-10-01 17:10:42 +0800

    iterm2: polish the iterm2-utilities package

--- modules/iTerm2/iterm2-utilities.nix
+++ modules/iTerm2/iterm2-utilities.nix
@@ -1,20 +1,19 @@
-{ stdenvNoCC, fetchFromGitHub, ... }:
+{ stdenvNoCC, fetchFromGitHub }:
 
 stdenvNoCC.mkDerivation rec {
   pname = "iterm2-utilities";
-  version = "46f6841";
+  version = "3.5";
 
   src = fetchFromGitHub {
     owner = "gnachman";
     repo = "iterm2-shell-integration";
     # 3.5 is the branch name, not the tag name.
-    rev = version;
-    hash = "sha256-RKUiUcdvkhaiQIosjWvVeYrF0g6Wk58QP7ROf2o5muo=";
+    rev = "e50c42051f8c9408c923a18d1406a5bc05414870";
+    hash = "sha256-9crDf4GC708cms/4z2EL7RFLCIdFaIODInKB7huTALU=";
   };
 
   # gnachman/iTerm2-shell-integration includes a Makefile with unknown usage.
   dontBuild = true;
-  dontFixup = true;
 
   installPhase = ''
     runHook preInstall
