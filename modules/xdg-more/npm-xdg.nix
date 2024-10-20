c299dfbcaa6066e280c2a8f2d6f4da00350bca9f
Bojun Ren <bj.ren.coding@outlook.com>
2024-10-20 22:46:51 +0800

    xdg(npm): use default config file of NPM
    By default NPM uses ~/.npmrc for per-user configuration. Setting environment
    variable NPM_CONFIG_USERCONFIG can change this. However, it's hard to guarantee
    the env can be set in all use cases... For convenience, just use the default
    configuration file.

--- modules/xdg-more/npm-xdg.nix
+++ modules/xdg-more/npm-xdg.nix
@@ -18,15 +18,17 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.sessionVariables = {
-      NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
-    };
-
-    # https://wiki.archlinux.org/title/XDG_Base_Directory
-    xdg.configFile."npm/npmrc".text = ''
+    # NPM weakly support XDG... Setting envs seems to work but for convenience I
+    # still use the default per-user configuration file. Options are set
+    # according to https://wiki.archlinux.org/title/XDG_Base_Directory.
+    home.file.".npmrc".text = ''
+      ; npm install -g
       prefix=${config.xdg.dataHome}/npm
+      ; npm cache
       cache=${config.xdg.cacheHome}/npm
+      ; npm init
       init-module=${config.xdg.configHome}/npm/config/npm-init.js
+      ; logs
       logs-dir=${config.xdg.stateHome}/npm/logs
     '';
   };
