dcec9a80225cf79ad781763829e8d83309c9600b
Bojun Ren <bj.ren.coding@outlook.com>
2024-12-31 13:37:29 +0800

    flake: add nix-vscode-extensions
    The flake nix-vscode-extensions provides almost all extensions for VS Code, more
    than Nixpkgs.

--- flake.nix
+++ flake.nix
@@ -8,6 +8,13 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Nixpkgs contains only a small fraction of VS Code extensions in marketplace.
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+
     flake-utils.url = "github:numtide/flake-utils";
 
     nur-rennsax = {
