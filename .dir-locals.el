e49e81e2cc78b55a07207e45b73cc1b8526f555d
Bojun Ren <bj.ren.coding@outlook.com>
2025-01-15 15:52:07 +0800

    chore: remove unnecessary dir-locals
    Cheatsheets are moved to https://github.com/rennsax/cheatsheets.git.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((auto-mode-alist . (("zshrc-*" . sh-mode)
-                     ("modules/cheat/cheatsheets/*" . sh-mode)))
- )
+((auto-mode-alist . (("zshrc-*" . sh-mode))))
