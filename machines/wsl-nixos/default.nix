271104360e2660111db4ea16e42f5287675ccca9
Bojun Ren <bj.ren.coding@outlook.com>
2025-01-26 10:22:51 +0800

    wsl: add user configuration (copied from lab-server)

--- machines/wsl-nixos/default.nix
+++ machines/wsl-nixos/default.nix
@@ -1,13 +1,21 @@
 {
   nixos-wsl,
+  home-manager,
   nixpkgs,
+  homeManagerModule,
+  myVars,
 }:
 {
   nixosConfigurations = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
       nixos-wsl.nixosModules.wsl
+      home-manager.nixosModules.home-manager
+
       (import ./nixos-config.nix)
     ];
+    specialArgs = {
+      inherit homeManagerModule myVars;
+    };
   };
 }
--- machines/wsl-nixos/nixos-config.nix
+++ machines/wsl-nixos/nixos-config.nix
@@ -1,5 +1,53 @@
-# Configuration for the WSL NixOS.
-{ pkgs, ... }:
+# Configuration for the WSL NixOS. Home-manager configuration is integrated in
+# this file, since there is no need to support multiple users in WSL.
+{
+  pkgs,
+  homeManagerModule,
+  myVars,
+  ...
+}:
+let
+  username = "rbj";
+  userConfig =
+    { pkgs, ... }:
+    {
+      imports = [
+        homeManagerModule
+      ];
+      myModules = {
+        git.enable = true;
+        zsh = {
+          enable = true;
+          dotDir = ".config/zsh";
+          plugins = [
+            "zsh-syntax-highlighting"
+            "zsh-autosuggestions"
+            "zsh-completions"
+          ];
+        };
+        fzf.enable = true;
+        z-lua = {
+          enable = true;
+          enableAliases = true;
+        };
+      };
+
+      programs.git = {
+        userName = myVars.me.userFullName;
+        userEmail = myVars.me.userEmail;
+      };
+
+      programs.starship = {
+        enable = true;
+        enableZshIntegration = true;
+        enableBashIntegration = true;
+      };
+
+      systemd.user.enable = false;
+
+      home.stateVersion = "24.05";
+    };
+in
 {
   wsl.enable = true;
   wsl.defaultUser = "nixos";
@@ -29,12 +77,14 @@
   };
 
   users.users = {
-    rbj = {
+    "${username}" = {
       isNormalUser = true;
       shell = pkgs.zsh;
       extraGroups = [ "wheel" ];
     };
   };
+
+  home-manager.users."${username}" = userConfig;
 
   system.stateVersion = "24.05";
 }
