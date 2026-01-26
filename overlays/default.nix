3323a8ab766760037fbce11e2ce997fede3863d2
Bojun Ren <me.rennsax@gmail.com>
2026-01-26 11:00:03 +0800

    chore: fix wrong filename (harmmerspoon -> hammerspoon)

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,7 +7,7 @@
     sarasa-term-sc-nerd = prev.callPackage ./sarasa-term-sc-nerd.nix { };
     gnu-coding-standards = prev.callPackage ./gnu-coding-standards.nix { };
     macos-trash = prev.callPackage ./macos-trash.nix { };
-    hammerspoon-macos = prev.callPackage ./harmmerspoon.nix { };
+    hammerspoon-macos = prev.callPackage ./hammerspoon.nix { };
 
     nix-vscode-extensions = nix-vscode-extensions.extensions.${final.stdenv.hostPlatform.system};
   })
