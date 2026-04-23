90de7da3200fc483427553a8878490e99d92a3cb
Bojun Ren <me.rennsax@gmail.com>
2026-04-23 19:53:31 +0800

    vterm: upgrade module

--- modules/emacs/pkgs/vterm-module/default.nix
+++ modules/emacs/pkgs/vterm-module/default.nix
@@ -10,12 +10,12 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "emacs-libvterm-module";
-  version = "d9ea29f";
+  version = "54c29d1";
   src = fetchFromGitHub {
     owner = "akermu";
     repo = "emacs-libvterm";
     rev = version;
-    hash = "sha256-rm7SjQSH50RyWt2AnTJg/gMNgA+btONjQWTEkbbywuA=";
+    hash = "sha256-ZSkGvKPhX4yZ2HNeNuHI7a01kjzBNBTdsWTj7y/xi8s=";
   };
   nativeBuildInputs = [ cmake ];
   buildInputs = [ libvterm-neovim ];
