704f31396298a64e65b27e11995495f4e681b30f
Bojun Ren <me.rennsax@gmail.com>
2026-05-28 14:23:19 +0800

    iterm2-shell-integration: 3.6.6 -> 3.6.10

--- modules/iTerm2/iterm2-shell-integration.nix
+++ modules/iTerm2/iterm2-shell-integration.nix
@@ -6,14 +6,14 @@ let
   hashes = {
     zsh = "sha256-kQJ8bVIh7nEjYJ6OWqiEDqIY+YWD5RbD1CXV+KKyDno=";
     bash = "sha256-v9G7xQtts7QDdctlwPpRbPcRvk+TYndLHDX9BbHxW5o=";
-    fish = "sha256-775Ie/ERim2ialQGYjwyJwT+hVHBzgFEniBai9sKz/0=";
+    fish = "sha256-++OLTyOdf0yxeub0DuzIPwu1Ois/zcDal+YvzD3Sjvk=";
   };
   shells = builtins.attrNames hashes;
   siFor =
     shell:
     fetchurl rec {
       pname = "iterm2-${shell}-integration";
-      version = "3.6.6";
+      version = "3.6.10";
       url = "https://gitlab.com/gnachman/iterm2/-/raw/v${version}/Resources/shell_integration/iterm2_shell_integration.${shell}";
       hash = hashes.${shell};
     };
